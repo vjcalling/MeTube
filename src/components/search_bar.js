b4c2bd17fcc89ffd9836a7f8d2035692f80c3db5
vjcalling <vibhor.jain@outlook.com>
2018-11-01 09:55:49 +0530

    adding css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -18,7 +18,7 @@ class SearchBar extends Component {
 
     render(){
         return(
-            <div>
+            <div className='search-bar'>
                 <input 
                     value={this.state.term}
                     onChange={this.onInputChange}

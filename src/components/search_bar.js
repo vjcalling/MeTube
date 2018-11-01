575807edbf42ad16bca9e8e00e0ee68878a8a8cf
vjcalling <vibhor.jain@outlook.com>
2018-11-01 09:43:41 +0530

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,37 @@
+
+import React, {Component} from 'react';
+
+// const SearchBar = () => {
+//     return <input />
+// }
+
+class SearchBar extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    render(){
+        return(
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+            </div>
+        );
+    }
+
+    onInputChange(event) {
+        this.setState({
+            term: event.target.value
+        });
+    }
+}
+
+export default SearchBar;
